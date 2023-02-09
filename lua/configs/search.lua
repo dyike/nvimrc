524cfaf355c839e83e6ffe1475c3683de9794d4a
dyike <yuanfeng634@gmail.com>
2023-02-09 16:39:38 +0800

    fix search keys

--- lua/configs/search.lua
+++ lua/configs/search.lua
@@ -3,8 +3,9 @@ local _M = {}
 function _M.config()
     -- fuzzy finder
     -- brew install ripgrep
-    vim.api.nvim_set_keymap('n', '<C-g>', "<cmd>Telescope live_grep<cr>", {noremap = true})
-    vim.api.nvim_set_keymap('n', '<C-s>', "<cmd>Telescope find_files<cr>", {noremap = true})
+    vim.api.nvim_set_keymap('n', '<leader>ff', "<cmd>Telescope live_grep<cr>", {noremap = true})
+    vim.api.nvim_set_keymap('n', '<leader>fg', "<cmd>Telescope find_files<cr>", {noremap = true})
+    vim.api.nvim_set_keymap('n', '<leader>fb', "<cmd>Telescope buffers<cr>", {noremap = true})
 end
 
 return _M
